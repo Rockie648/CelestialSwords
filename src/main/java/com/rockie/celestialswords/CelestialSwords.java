a52ebf90620b69bbb79062d675fa1096db2ffef0
Rockie648 <pathaksugam468@gmail.com>
2025-12-09 17:31:28 +0545

    Update CelestialSwords.java

--- src/main/java/com/rockie/celestialswords/CelestialSwords.java
+++ src/main/java/com/rockie/celestialswords/CelestialSwords.java
@@ -16,27 +16,67 @@ public class CelestialSwords extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Celestial Swords plugin has been enabled!");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("Celestial Swords plugin has been disabled!");
+        getLogger().info("Celestial Swords plugin enabled!");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        // Permission check
         if (!sender.hasPermission("celestial.admin")) {
-            sender.sendMessage("§cYou do not have permission to use this command!");
+            sender.sendMessage("§cYou must be an operator to use this command!");
             return true;
         }
 
         if (command.getName().equalsIgnoreCase("celestial")) {
 
-            // Self-give: /celestial
-            if (args.length == 0) {
-                if (sender instanceof Player player) {
-                    giveAllSwords(player);
-                    sender.sendMessage("§aYou received all Ce
+            if (args.length == 0 && sender instanceof Player player) {
+                giveAllSwords(player);
+                return true;
+            }
+
+            if (args.length == 1) {
+                Player target = Bukkit.getPlayer(args[0]);
+                if (target != null) {
+                    giveAllSwords(target);
+                    sender.sendMessage("§aGiven all swords to " + target.getName());
+                } else {
+                    sender.sendMessage("§cPlayer not found!");
+                }
+                return true;
+            }
+
+            sender.sendMessage("§cUsage: /celestial [player]");
+            return true;
+        }
+
+        return false;
+    }
+
+    private void giveAllSwords(Player player) {
+        player.getInventory().addItem(createKurozai());
+        player.getInventory().addItem(createZanpakuto());
+        player.getInventory().addItem(createIceGlacial());
+        player.sendMessage("§aYou received all Celestial Swords!");
+    }
+
+    // ⚔️ Kurozai
+    private ItemStack createKurozai() {
+        ItemStack sword = new ItemStack(Material.NETHERITE_SWORD);
+        ItemMeta meta = sword.getItemMeta();
+        meta.setDisplayName("§5Kurozai");
+        List<String> lore = new ArrayList<>();
+        lore.add("§7Pulls everything into a dark vortex!");
+        meta.setLore(lore);
+        sword.setItemMeta(meta);
+        return sword;
+    }
+
+    // 🔥 Zanpakuto of Fire
+    private ItemStack createZanpakuto() {
+        ItemStack sword = new ItemStack(Material.DIAMOND_SWORD);
+        ItemMeta meta = sword.getItemMeta();
+        meta.setDisplayName("§cZanpakuto of Fire");
+        List<String> lore = new ArrayList<>();
+        lore.add("§7Unleashes fiery explosions!");
+        meta.setLore(lore);
+        sword.setItemMeta(meta);
